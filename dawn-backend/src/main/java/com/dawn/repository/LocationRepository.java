21d19e3b80c5987afdaf9b29fd4d839e641fbec1
ji-mim <194115@jnu.ac.kr>
2025-05-04 16:03:50 +0900

    refactor: 쿼리문 수정 검색어를 기준으로 검색어가 Location 키워드에 포함되어 있으면 검색되도록 작성했음

--- dawn-backend/src/main/java/com/dawn/repository/LocationRepository.java
+++ dawn-backend/src/main/java/com/dawn/repository/LocationRepository.java
@@ -13,9 +13,11 @@ public interface LocationRepository extends JpaRepository<Location, Long> {
 
 
     //쿼리문 수정해야할듯
-    @Query("SELECT l FROM Location l WHERE " +
-            "LOWER(l.name) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
-            "OR LOWER(l.address) LIKE LOWER(CONCAT('%', :keyword, '%'))")
-
-    List<Location> searchByKeyword(@Param("keyword") String keyword);
-}
+    @Query("""
+    SELECT DISTINCT l
+    FROM Location l
+    JOIN Keyword k ON k.location = l
+    WHERE (:keyword IS NOT NULL AND :keyword <> '')
+      AND LOWER(k.keyword) LIKE LOWER(CONCAT('%', :keyword, '%'))
+    """)
+    List<Location> searchByKeyword(@Param("keyword") String keyword);}
