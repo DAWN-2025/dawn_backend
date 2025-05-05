a6830d31b68a5cd781fd973a2af3834cb1fcf509
ji-mim <194115@jnu.ac.kr>
2025-05-05 17:52:14 +0900

    refactor: repository 기능 수정
    로케이션을 id를 기준으로 방명록을 찾고, 삭제됐으면 보이지 않게함

--- dawn-backend/src/main/java/com/dawn/service/dto/VisitedResponse.java
+++ dawn-backend/src/main/java/com/dawn/service/dto/VisitedResponse.java
@@ -9,21 +9,23 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 public class VisitedResponse {
-    private Long seq;
-    private Long userSeq;
-    private Long locationSeq;
-    private LocalDateTime visitedDate;
+    private Long id;
+    private String userEmail;
     private String comment;
+    private LocalDateTime createdAt;
+    private boolean edited;
     private int likes;
+    private boolean myComment;
 
-    public static VisitedResponse from(Visited v) {
+    public static VisitedResponse from(Visited visited, String currentUserEmail) {
         return VisitedResponse.builder()
-                .seq(v.getSeq())
-                .userSeq(v.getUser().getSeq())
-                .locationSeq(v.getLocation().getSeq())
-                .visitedDate(v.getVisitedDate())
-                .comment(v.getComment())
-                .likes(v.getLikes())
+                .id(visited.getSeq())
+                .userEmail(visited.getUser().getEmail())
+                .comment(visited.getComment())
+                .createdAt(visited.getCreatedAt())
+                .edited(visited.isEdited())
+                .likes(visited.getLikes())
+                .myComment(visited.getUser().getEmail().equals(currentUserEmail))
                 .build();
     }
 }
