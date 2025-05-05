c39456f6ef8526265f04823d4e52804c1df756dd
ji-mim <194115@jnu.ac.kr>
2025-05-05 15:50:05 +0900

    refactor: Location 엔티티 필드 추가

--- dawn-backend/src/main/java/com/dawn/domain/Location.java
+++ dawn-backend/src/main/java/com/dawn/domain/Location.java
@@ -3,6 +3,10 @@ package com.dawn.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "LOCATION_TBL")
 @Getter
@@ -30,14 +34,40 @@ public class Location {
     @Column(name = "LOC_IMAGE")
     private String image;
 
-    @Column(name = "LOC_INFO")
-    private String info;
+    @OneToMany(mappedBy = "location")
+    @Builder.Default
+    private List<Keyword> keywords = new ArrayList<>();
+
+
+    @Column(name = "LOC_SHORT_INFO")
+    private String shortInfo;
+
+    @Column(name = "LOC_HISTORIC_INFO")
+    private String historicInfo;
+
+    @Column(name = "LOC_ETIQUETTE")
+    private String etiquette;
+
+    @Column(name = "LOC_OPEN_TIME")
+    private LocalDateTime openTime;
+
+    @Column(name = "LOC_CLOSE_TIME")
+    private LocalDateTime closeTime;
+
+    @Column(name = "LOC_PHONE_NUM")
+    private String phoneNum;
+
+    @Column(name = "EXHIBITION_TIME")
+    private String exhibitionTIme;
+
+    @Column(name = "AVAILABLE")
+    private String available;
+
+    @Column(name = "TRANSLATE")
+    private String translate;
 
     @Column(name = "LOC_NAME_ENG")
     private String nameEng;
-
-    @Column(name = "LOC_INFO_ENG")
-    private String infoEng;
 
     @Column(name = "LOC_ADDRESS_ENG")
     private String addressEng;
