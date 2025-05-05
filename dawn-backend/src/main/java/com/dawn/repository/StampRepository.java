d01e85ffa9b5c34173feeaaaa3428255d6b50974
ji-mim <194115@jnu.ac.kr>
2025-05-05 22:23:55 +0900

    refactor: 유저가 해당 로케이션에 스탬프를 가지고 있는지 호가인하는 메서드 추가

--- dawn-backend/src/main/java/com/dawn/repository/StampRepository.java
+++ dawn-backend/src/main/java/com/dawn/repository/StampRepository.java
@@ -1,5 +1,6 @@
 package com.dawn.repository;
 
+import com.dawn.domain.Location;
 import com.dawn.domain.Stamp;
 import com.dawn.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,6 @@ import java.util.List;
 
 public interface StampRepository extends JpaRepository<Stamp, Long> {
     List<Stamp> findByOwner(User user);
+
+    boolean existsByOwnerAndLocation(User owner, Location location);
 }
